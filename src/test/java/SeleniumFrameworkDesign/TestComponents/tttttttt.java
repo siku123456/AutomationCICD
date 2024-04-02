415f5afe59a4cd6913904146220c3d4ce6899480
siku <dehuri.smruti@gmail.com>
2024-04-01 19:54:28 -0700

    cicd first commit

---
+++ src/test/java/SeleniumFrameworkDesign/TestComponents/tttttttt.java
@@ -0,0 +1,84 @@
+package SeleniumFrameworkDesign.TestComponents;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import SeleniumFrameworkDesign.resources.ExtentReporterNG;
+
+public class tttttttt extends BaseTest implements ITestListener{
+	
+	//System.out.println("Test passed");
+	
+	ExtentReports extent=ExtentReporterNG.getReportObject();
+	ExtentTest test;
+	ThreadLocal<ExtentTest> extentTest=new ThreadLocal<ExtentTest>();
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		test=extent.createTest(result.getMethod().getMethodName());
+		extentTest.set(test);//unique threadID-test
+		System.out.println("Test Listener");
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		extentTest.get().log(Status.PASS, "Test Passed1");
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		extentTest.get().fail(result.getThrowable());
+		String filePath = null;
+		
+		try {
+			driver=(WebDriver) result.getTestClass().getRealClass().getField("driver").get(result.getInstance());
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		
+		}
+		try {
+			filePath = getScreenshot(result.getMethod().getMethodName(),driver);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		extentTest.get().addScreenCaptureFromPath(filePath,result.getMethod().getMethodName());
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		extent.flush();
+	}
+
+}
